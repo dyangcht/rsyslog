fc0b56ddb981f5d13f7fcbdcf51154f14f70db22
David <yangdavid@365office.com.tw>
2021-05-11 16:42:11 +0800

    Update verbose.lua

--- rsyslog/verbose.lua
+++ rsyslog/verbose.lua
@@ -9,12 +9,12 @@ local mt = { __index = setmetatable(_M, { __index = apicast }) }
 function _M.new(configuration)
   local config = configuration or {}
   local set_header = config.set_header or {}
-  -- local set_body = config.set_body or {}
+  local set_body = config.set_body or {}
   
   for _, header in ipairs(set_header) do
     ngx.log(ngx.WARN, "set header name: " .. header.name .. " value: " .. header.value)
   end
-  -- ngx.log(ngx.WARN, "new()..." .. set_body)
+  ngx.log(ngx.WARN, "new()..." .. set_body)
   ngx.log(ngx.WARN, "new()...")
   return setmetatable({}, mt)
 end
